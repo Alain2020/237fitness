55db6d7f964ef19932073d4eeb0d2199b46185bc
Alain2020 <favouralain@yahoo.com>
2022-11-08 23:41:44 +0000

    Add javescript to the portfolio and styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,31 @@ document.querySelector('#search-btn').onclick = () =>{
 window.onscroll = () =>{
     searchForm.classList.remove('active');
 }
+
+$(document).ready(function(){
+
+     //owl owlCarousel
+     $('.owl-carousel').owlCarousel({
+        margin: 5,
+        mavigation: true,
+        loop: true,
+        autoplay: true,
+        autopplayTimeOut: 2000,
+        autoplayHoverPause: true,
+
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav : false
+            }
+        }
+     });
+});
