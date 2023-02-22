25dd315bed21de22c282dede5bd0fcfcca33f4f5
Alain2020 <favouralain@yahoo.com>
2023-02-22 02:15:58 +0000

    Adding new picture

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,27 @@
-// SearchForm Section code
-let searchForm = document.querySelector('.search-form');
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-}
-window.onscroll = () =>{
-    searchForm.classList.remove('active');
-}
+var menu = document.getElementById("bar");
+var item = document.getElementById("item");
 
-$(document).ready(function(){
+item.style.right = '-300px';
+menu.onclick = function () {
+    /*----body---*/
+    if (item.style.right == '-300px') {
+        item.style.right = '0';
+    }
+    else {
+        item.style.right = '-300px';
+    }
+    }
 
-    $(window).scroll(function(){
-        //Navbar-bottom scrolling
-        if(this.scrolly > 5){
-            $('.navbar-bottom').addClass("sticky");
-        }else{
-            $('.navbar-bottom').removeClass("sticky");
-        }
 
-        //Scrolling Buttom Btn
-        if(thi.scrollY > 500){
-            $('.navbar-bottom').addClass("sticky");
-        }else{
-            $('.navbar-bottom').removeClass("show"); 
-        }
-    });
 
-    //slide up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop:0});
-    });
 
-     //owl owlCarousel
-     $('.owl-carousel').owlCarousel({
-        margin: 5,
-        mavigation: true,
-        loop: true,
-        autoplay: true,
-        autopplayTimeOut: 2000,
-        autoplayHoverPause: true,
+//*--- Open Menu --- *//
 
-        responsive: {
-            0: {
-                items: 1,
-                nav: false
-            },
-            600: {
-                items: 2,
-                nav: false
-            },
-            1000: {
-                items: 3,
-                nav : false
-            }
-        }
-     });
-});
+
+//**--- Close Menu ---**//
+
+
+//*-- Scroll Sticky nav -- *//
+
+
+//****--- Scroll Top ----****//
