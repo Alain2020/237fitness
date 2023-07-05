fa7df6b98a18e4bf124acb038086238ce7017bfd
Alain2020 <favouralain@yahoo.com>
2023-07-05 19:04:00 +0000

    Update CSS, JS, and HTML files

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,13 @@ var menu = document.getElementById("bar");
 var item = document.getElementById("item");
 
 item.style.right = '-300px';
-menu.onclick = function () {
-    /*----body---*/
-    if (item.style.right == '-300px') {
-        item.style.right = '0';
-    }
-    else {
-        item.style.right = '-300px';
-    }
-    }
-
-
-
+menu.addEventListener("click", function () {
+  if (item.style.right === '-300px') {
+    item.style.right = '0';
+  } else {
+    item.style.right = '-300px';
+  }
+});
 
 //*--- Open Menu --- *//
 
